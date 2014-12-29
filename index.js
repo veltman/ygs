8561292926b6a426edf29f8470f86c6cc69a7bee
Noah <noah@noahveltman.com>
2014-12-29 11:43:57 -0500

    Adding body-parser for POST data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #! /usr/bin/env node
 
 var express = require("express"),
+    parser = require("body-parser"),
     app = express(),
     directory = require("serve-index");
 
+app.use(parser);
 app.use(express.static(__dirname));
 app.use(directory(__dirname));
 
